ad96ada691fbb4603d6b8935899a3291c2366473
= <=>
2018-10-14 19:32:21 +0500

    database email

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ class EDIAccount(models.Model):
     ipHost = models.CharField(max_length=250)
     userName = models.CharField(max_length=500)
     password = models.CharField(max_length=100)
-    email = models.CharField(max_length=500,  default='emailAddress')
+    # email = models.CharField(max_length=500,  default='emailAddress')
     input_path = models.CharField(max_length=1000)
     output_path = models.CharField(max_length=1000)
     ip_hostOut = models.CharField(max_length=250)
